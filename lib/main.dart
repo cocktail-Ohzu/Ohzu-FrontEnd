fd1ddc4de16d63072ab37d0d793be3777373de11
gengminy <kls1998@naver.com>
2022-03-17 18:38:56 +0900

    메인 페이지 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,36 +11,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const CupertinoApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: '오쥬'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +28,167 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors:[
+            Color(0xff6F3929),
+            Color(0xff3A1A18),
+            Color(0xff191616),
+            Color(0xff121212),
+            Color(0xff2A1010),
           ],
+        )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: CupertinoNavigationBar(
+          backgroundColor: Colors.transparent,
+          border: const Border(bottom: BorderSide(color: Colors.transparent)),
+          //middle: Text(widget.title),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, size: 25,),
+            color: Colors.white.withOpacity(0.6),
+          )
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(20, 0, 20, 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+               /* 추천 칵테일 타이틀 */
+               Container(
+                 alignment: Alignment.centerLeft,
+                 child: const Text(
+                   "오늘의 추천 칵테일",
+                   style: TextStyle(
+                     fontWeight: FontWeight.w500,
+                     fontSize: 18,
+                     color: Colors.white,
+                   ),
+                 ),
+                 margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+               ),
+
+               /* 추천 칵테일 컨테이너 */
+               Container(
+                 margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                 decoration: const BoxDecoration(
+                   gradient: RadialGradient(
+                     colors:[
+                       Color(0xff513535),
+                       Color(0xffF28093),
+                     ],
+                     center: Alignment(1.2, -0.2),
+                     focal: Alignment(-0.2, 0.4),
+                     focalRadius: 1,
+                   ),
+                   borderRadius: BorderRadius.all(Radius.circular(25)),
+                 ),
+                 height: 460,
+                 child: Column(
+                   children: [
+                     /* 추천 칵테일 이미지 */
+                     Container(
+                       margin: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+                       width: double.infinity,
+                       height: 350,
+                       decoration: const BoxDecoration(
+                         color: Color(0xffF28093),
+                         borderRadius: BorderRadius.all(Radius.circular(25)),
+                       ),
+                     ),
+
+                     /* 추천 칵테일 텍스트 */
+                     Container(
+                       margin: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                       alignment: Alignment.centerLeft,
+                       child: Column(
+                         mainAxisAlignment: MainAxisAlignment.start,
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
+                           const SizedBox(height: 5),
+                           Text.rich(
+                             TextSpan(
+                               style: const TextStyle(
+                                 fontWeight: FontWeight.bold,
+                                 fontSize: 22,
+                                 color: Colors.white,
+                               ),
+                               children: <TextSpan>[
+                                 TextSpan(text: "미도리 사워"),
+                                 TextSpan(text: " | ", style: TextStyle(
+                                   fontWeight: FontWeight.w900,
+                                   color: Colors.white.withOpacity(0.5),
+                                   fontSize: 25,
+                                 )),
+                                 TextSpan(text: "Midori Sour", style: TextStyle(
+                                   fontWeight: FontWeight.w500,
+                                   color: Colors.white.withOpacity(0.5),
+                                   fontSize: 16,
+                                 )),
+                               ],
+                             )
+                           ),
+                           Text("90년대를 휩쓸었던 전설의 그 칵테일!",
+                           style: TextStyle(
+                             height: 1.5,
+                             fontSize: 15,
+                             fontWeight: FontWeight.w900,
+                             color: Colors.white.withOpacity(0.7),
+                           )),
+                           const SizedBox(height: 15,),
+                           Text("alcohol 20%",
+                               style: TextStyle(
+                                 fontSize: 12,
+                                 fontWeight: FontWeight.w500,
+                                 color: Colors.deepOrangeAccent.withOpacity(0.8),
+                               )
+                           ),
+                         ],
+                       )
+                     )
+                   ],
+                 ),
+               ),
+
+                const SizedBox(height: 10,),
+               /* 하단 버튼 1 */
+               CupertinoButton(
+                 color: const Color(0xffA35C65),
+                   child: Text("자세한 정보가 궁금해요",
+                   style: TextStyle(
+                     color: const Color(0xffDCC5B6).withOpacity(0.8),
+                     fontSize: 13,
+                     fontWeight: FontWeight.w900
+                   ),
+                 ),
+                 onPressed: (){},
+               ),
+
+                const SizedBox(height: 10,),
+                /* 하단 버튼 2 */
+                CupertinoButton(
+                  color: const Color(0xff351B1B),
+                  child: const Text("나에게 맞는 칵테일로 추천 받을래요",
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onPressed: (){},
+                ),
+             ],
+            ),
+          )
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-}
+  }}
